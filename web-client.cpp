9ca5276d8cb7d43a5f927758e124c8fe80507b79
SophieHegarty <shegarty874@gmail.com>
2018-02-17 20:09:11 +0000

    updated

--- web-client.cpp
+++ web-client.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #define TM_BUF_SIZE 20
 //#define TM_PACKETS_TO_SEND 10
 //#define TM_DEST_ADDR “10.129.36.52”
-#define TM_DEST_PORT 4000
+#define TM_DEST_PORT 4001
 
 int main()
 {
@@ -30,13 +30,15 @@ int main()
     //destination port
     destination_address.sin_port = htons(TM_DEST_PORT);
     //ip address decleration
-    destination_address.sin_addr.s_addr = inet_addr("0.0.0.0");
+    destination_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    memset(destination_address.sin_zero, '\0', sizeof(destination_address.sin_zero));
     
     //socket creation
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     
     //verify?  dont do this, idk how
-    if(clientSocket != 0 ){
+    if(clientSocket == -1 ){
         perror("socket not created");
         return 1;
     }
@@ -44,7 +46,7 @@ int main()
     //connect to the server not wokring??????????
     error = connect(clientSocket,(struct sockaddr *)&destination_address, sizeof(destination_address));
     
-    if(error < 0){
+    if(error == -1){
         perror("Socket not connected");
         return 2;
     }
@@ -52,14 +54,14 @@ int main()
     struct sockaddr_in  client_adress;
     socklen_t client_length = sizeof(client_adress);
     int socknamecode = getsockname(clientSocket, (struct sockaddr *)&client_adress, &client_length);
-    if(socknamecode < 0){
+    if(socknamecode == -1){
         perror("get sock name");
         return 3;
     }
     
     char ip_string[INET_ADDRSTRLEN] = {'\0'};
     inet_ntop(client_adress.sin_family, & client_adress.sin_addr, ip_string, sizeof(ip_string));
-    cout << "Connection set up from: " << ip_string << ntohs(client_adress.sin_port) << "\n";
+    cout << "Connection set up from: " << ip_string << ":" << ntohs(client_adress.sin_port) << "\n";
     
     //seend/receive data
     bool end = false;
@@ -79,12 +81,16 @@ int main()
             perror("send");
             return 4;
         }
-    }//end while end
+        
         long recvCheck = recv(clientSocket, buffer, TM_BUF_SIZE, 0);
-        if(recvCheck < 0 ){
+        if(recvCheck == -1 ){
             perror("receive");
             return 5;
         }
+        cout << "Received " << buffer << "\n";
+        
+    }//end while end
+        
         close(clientSocket);
     }//end while 1
     
